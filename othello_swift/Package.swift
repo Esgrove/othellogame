160fb0fc5bec718e23bce6faa8fe77b1ff241187
Esgrove <esgrove@outlook.com>
2022-11-03 13:33:22 +0200

    update swift package

--- othello_swift/Package.swift
+++ othello_swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "othello_swift",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.5.0"),
+        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
