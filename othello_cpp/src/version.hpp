a9080c11dd1da821a3bbbc51627b0d3cca02857b
Victor Balakine <victor@yousician.com>
2023-04-12 15:19:10 +0300

    Use compile definitions to pass build info

---
+++ othello_cpp/src/version.hpp
@@ -0,0 +1,10 @@
+#pragma once
+namespace version
+{
+constexpr auto APP_NAME = BUILDTIME_APP_NAME;
+constexpr auto BRANCH = BUILDTIME_BRANCH;
+constexpr auto BUILD_NAME = BUILDTIME_BUILD_NAME;
+constexpr auto COMMIT = BUILDTIME_COMMIT;
+constexpr auto DATE = BUILDTIME_DATE;
+constexpr auto VERSION_NUMBER = BUILDTIME_VERSION_NUMBER;
+}  // namespace version
