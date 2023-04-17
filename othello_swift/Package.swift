ddf64b8a15a2c3d859c2924a8e01f54932601395
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-04-17 09:49:53 +0300

    add ArgumentParser for swift

--- othello_swift/Package.swift
+++ othello_swift/Package.swift
@@ -8,13 +8,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "othello_swift",
-            dependencies: ["ColorizeSwift"]
+            dependencies: [
+                .product(name: "ColorizeSwift", package: "ColorizeSwift"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
         ),
     ]
 )
