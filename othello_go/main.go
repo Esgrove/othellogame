fb40e86066b66042301facf8465156fb846084b2
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-08-23 14:23:26 +0300

    move version info function to utils

--- othello_go/main.go
+++ othello_go/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime/debug"
 	"slices"
 	"strconv"
 
@@ -11,33 +10,11 @@ import (
 	"github.com/logrusorgru/aurora/v4"
 )
 
-var versionInfo = func() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		version := info.GoVersion
-		commit := ""
-		timestamp := ""
-		arch := ""
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				commit = setting.Value
-			}
-			if setting.Key == "vcs.time" {
-				timestamp = setting.Value
-			}
-			if setting.Key == "GOARCH" {
-				arch = setting.Value
-			}
-		}
-		return fmt.Sprintf("Othello Go %s %s %s %s", timestamp, commit, version, arch)
-	}
-	return ""
-}()
-
 func main() {
 	fmt.Println(aurora.Green("OTHELLO GAME - GO").Bold())
 	args := os.Args[1:]
 	if slices.Contains(args, "--help") || slices.Contains(args, "-h") {
-		fmt.Println(versionInfo)
+		fmt.Println(versionInfo())
 		fmt.Println("USAGE: othello_go [board size]")
 		fmt.Println("\nOptional arguments:")
 		fmt.Println("    -h | --help          Print usage and exit")
@@ -45,7 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 	if slices.Contains(args, "--version") || slices.Contains(args, "-v") {
-		fmt.Println(versionInfo)
+		fmt.Println(versionInfo())
 		os.Exit(0)
 	}
 	boardSize, err := strconv.Atoi(args[0])
--- othello_go/utils.go
+++ othello_go/utils.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func versionInfo() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		version := info.GoVersion
+		commit := ""
+		timestamp := ""
+		arch := ""
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				commit = setting.Value
+			}
+			if setting.Key == "vcs.time" {
+				timestamp = setting.Value
+			}
+			if setting.Key == "GOARCH" {
+				arch = setting.Value
+			}
+		}
+		return fmt.Sprintf("Othello Go %s %s %s %s", timestamp, commit, version, arch)
+	}
+	return ""
+}
