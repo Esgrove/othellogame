2d113e201c3ebbb0854dd7a192a876e8bf9639eb
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-08-23 23:38:17 +0300

    add separate main for cpp

---
+++ othello_cpp/src/main.cpp
@@ -0,0 +1,68 @@
+//==========================================================
+// Main
+// Play Othello (Reversi) on the command line
+// https://en.wikipedia.org/wiki/Reversi
+// Akseli Lukkarila
+// 2019-2023
+//==========================================================
+
+#include "colorprint.hpp"
+#include "othello.hpp"
+#include "version.hpp"
+
+static constexpr size_t MIN_BOARD_SIZE = 4;
+static constexpr size_t MAX_BOARD_SIZE = 16;
+
+int main(int argc, const char* argv[])
+{
+    print_bold("OTHELLO GAME - C++\n", fmt::color::green);
+    std::vector<std::string> arguments(argv, argv + argc);
+
+    // Handle 'help' and 'version' arguments
+    for (const auto& arg : arguments) {
+        if (arg == "--help" || arg == "-h") {
+            fmt::print(
+                "{} {} {}\n\n"
+                "USAGE: othello_cpp [board size]\n\n"
+                "Optional arguments:\n"
+                "    -h | --help          Print usage and exit\n"
+                "    -v | --version       Print version info and exit\n",
+                version::APP_NAME,
+                version::VERSION_NUMBER,
+                version::DATE);
+            return 1;
+        }
+        if (arg == "--version" || arg == "-v") {
+            fmt::print(
+                "{} {} {} {} {}\n",
+                version::APP_NAME,
+                version::VERSION_NUMBER,
+                version::DATE,
+                version::BRANCH,
+                version::COMMIT);
+            return 0;
+        }
+    }
+
+    // Try to read board size from command line args
+    size_t board_size;
+    try {
+        if (arguments.size() >= 2) {
+            board_size = std::stoi(arguments[1]);
+            if (board_size < MIN_BOARD_SIZE || board_size > MAX_BOARD_SIZE) {
+                print_error(fmt::format("Unsupported board size: {}", board_size));
+                return 1;
+            }
+            fmt::print("Using board size: {}\n", board_size);
+        } else {
+            throw std::invalid_argument("Invalid board size");
+        }
+    } catch (const std::invalid_argument&) {
+        // Otherwise ask user for size
+        board_size = othello::Othello::get_board_size();
+    }
+
+    othello::Othello game {board_size};
+    game.play();
+    return 0;
+}
--- othello_cpp/src/othello.cpp
+++ othello_cpp/src/othello.cpp
@@ -14,9 +14,6 @@
 #include <algorithm>  // clamp, transform
 #include <iostream>
 
-static constexpr size_t MIN_BOARD_SIZE = 4;
-static constexpr size_t MAX_BOARD_SIZE = 16;
-
 othello::Othello::Othello(size_t board_size) : board(Board(board_size)), board_size(board_size) {}
 
 /// Play one full game of Othello.
@@ -114,58 +111,4 @@ size_t othello::Othello::get_board_size()
     int size;
     std::cin >> size;
     return static_cast<size_t>(std::clamp(size, 4, 10));
-}
-
-int main(int argc, const char* argv[])
-{
-    print_bold("OTHELLO GAME - C++\n", fmt::color::green);
-    std::vector<std::string> arguments(argv, argv + argc);
-
-    // Handle 'help' and 'version' arguments
-    for (const auto& arg : arguments) {
-        if (arg == "--help" || arg == "-h") {
-            fmt::print(
-                "{} {} {}\n\n"
-                "USAGE: othello_cpp [board size]\n\n"
-                "Optional arguments:\n"
-                "    -h | --help          Print usage and exit\n"
-                "    -v | --version       Print version info and exit\n",
-                version::APP_NAME,
-                version::VERSION_NUMBER,
-                version::DATE);
-            return 1;
-        }
-        if (arg == "--version" || arg == "-v") {
-            fmt::print(
-                "{} {} {} {} {}\n",
-                version::APP_NAME,
-                version::VERSION_NUMBER,
-                version::DATE,
-                version::BRANCH,
-                version::COMMIT);
-            return 0;
-        }
-    }
-
-    // Try to read board size from command line args
-    size_t board_size;
-    try {
-        if (arguments.size() >= 2) {
-            board_size = std::stoi(arguments[1]);
-            if (board_size < MIN_BOARD_SIZE || board_size > MAX_BOARD_SIZE) {
-                print_error(fmt::format("Unsupported board size: {}", board_size));
-                return 1;
-            }
-            fmt::print("Using board size: {}\n", board_size);
-        } else {
-            throw std::invalid_argument("Invalid board size");
-        }
-    } catch (const std::invalid_argument&) {
-        // Otherwise ask user for size
-        board_size = othello::Othello::get_board_size();
-    }
-
-    othello::Othello game {board_size};
-    game.play();
-    return 0;
 }
