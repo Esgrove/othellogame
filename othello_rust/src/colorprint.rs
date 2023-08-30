c8bde7c685953b459f10fb0dc5663c23e6ac45a5
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-08-30 12:33:09 +0300

    rust: handle indent correctly in prints

--- othello_rust/src/colorprint.rs
+++ othello_rust/src/colorprint.rs
@@ -7,12 +7,16 @@
 
 use colored::Colorize;
 
+/// Print error message with red colour.
 pub fn print_error(message: &str) {
-    let full_message = format!("Error: {}", message);
-    println!("{}", full_message.red())
+    let indent_size = message.chars().take_while(|c| c.is_whitespace()).count();
+    let (indent, text) = message.split_at(indent_size);
+    println!("{}", format!("{}Error: {}", indent, text).red())
 }
 
+/// Print warning message with yellow colour.
 pub fn print_warn(message: &str) {
-    let full_message = format!("Warning: {}", message);
-    println!("{}", full_message.yellow())
+    let indent_size = message.chars().take_while(|c| c.is_whitespace()).count();
+    let (indent, text) = message.split_at(indent_size);
+    println!("{}", format!("{}Warning: {}", indent, text).yellow())
 }
