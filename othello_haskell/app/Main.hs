37397d2ff5fb65619017ca6b90e203a3fa14a279
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-11-10 19:24:43 +0200

    create haskell project

---
+++ othello_haskell/app/Main.hs
@@ -0,0 +1,59 @@
+module Main where
+
+import ColorPrint
+import Data.Semigroup ()
+import Data.Version (showVersion)
+import Options.Applicative
+-- Import version from Cabal package
+import Paths_othello (version)
+import System.Console.ANSI
+
+data Options = Options
+  { size :: Maybe Int,
+    autoplay :: Bool,
+    defaultSet :: Bool,
+    showLog :: Bool,
+    noHelpers :: Bool,
+    test :: Bool
+  }
+
+options :: Parser Options
+options =
+  Options
+    <$> optional
+      ( argument
+          auto
+          (metavar "SIZE" <> help "Optional board size (MIN_BOARD_SIZE..MAX_BOARD_SIZE)")
+      )
+    <*> switch (long "autoplay" <> short 'a' <> help "Enable autoplay mode")
+    <*> switch (long "default" <> short 'd' <> help "Play with default settings")
+    <*> switch (long "log" <> short 'l' <> help "Show log after a game")
+    <*> switch (long "no-helpers" <> short 'n' <> help "Hide disk placement hints")
+    <*> switch (long "test" <> short 't' <> help "Enable test mode")
+
+main :: IO ()
+main = do
+  printBold "OTHELLO GAME - Haskell" System.Console.ANSI.Green
+  execParser opts >>= runWithOptions
+  where
+    opts =
+      info
+        (helper <*> versionOption <*> options)
+        ( fullDesc
+            <> progDesc "A simple Othello CLI game implementation."
+            <> header "Othello Haskell"
+        )
+    versionOption =
+      infoOption
+        (showVersion version)
+        (long "version" <> short 'v' <> help "Print version and exit")
+
+runWithOptions :: Options -> IO ()
+runWithOptions opts = do
+  putStrLn "Parsed options:"
+  putStrLn $ "Size: " ++ show (size opts)
+  putStrLn $ "Autoplay: " ++ show (autoplay opts)
+  putStrLn $ "Default settings: " ++ show (defaultSet opts)
+  putStrLn $ "Log: " ++ show (showLog opts)
+  putStrLn $ "No helpers: " ++ show (noHelpers opts)
+  putStrLn $ "Test mode: " ++ show (test opts)
