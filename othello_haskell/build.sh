c1097ff2bf5bd4823356174ad9a952ef7a3b41c6
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-11-13 15:21:45 +0200

    add build script

---
+++ othello_haskell/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eo pipefail
+
+# Import common functions
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../common.sh
+source "$DIR/../common.sh"
+
+# Get absolute path to repo root
+REPO_ROOT=$(git rev-parse --show-toplevel || (cd "$(dirname "../${BASH_SOURCE[0]}")" && pwd))
+PROJECT_PATH="$REPO_ROOT/othello_haskell"
+
+print_magenta "Building Othello Haskell..."
+
+if [ -z "$(command -v cabal)" ]; then
+    print_error_and_exit "cabal not found in path"
+else
+    cabal --version
+fi
+
+pushd "$PROJECT_PATH" > /dev/null
+
+if [ "$PLATFORM" = windows ]; then
+    executable="othello_haskell.exe"
+else
+    executable="othello_haskell"
+fi
+
+rm -f "$executable"
+
+cabal build
+
+file "$executable"
+./"$executable" --version
+popd > /dev/null
