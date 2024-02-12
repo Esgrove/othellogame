87457c6a7c30164cb91ee18cf4790e95e3511416
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-02-12 19:15:04 +0200

    zig: full version info

--- zig/src/version.zig
+++ zig/src/version.zig
@@ -1,1 +1,5 @@
-pub const version = "0.1.0";
+// Generated automatically by build script; DO NOT EDIT MANUALLY.
+pub const BUILD_TIME = "2024-02-12_1914";
+pub const GIT_HASH = "9fc6254";
+pub const GIT_BRANCH = "zig";
+pub const VERSION = "0.1.0";
