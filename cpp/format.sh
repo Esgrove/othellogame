f9c01b660f4e4e71f0995e24261849ac3cb243ce
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-02-13 14:26:04 +0200

    shorten format script name

--- cpp/format.sh
+++ cpp/format.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Format C++ code using ClangFormat
 set -eo pipefail
+
+# Format C++ code using ClangFormat
 
 REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 clang-format --verbose -i -style=file "$REPO"/src/*.cpp "$REPO"/src/*.hpp
