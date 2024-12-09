d524729ad5ac852c32fd860ea7243ca39613724c
Esgrove <esgrove@outlook.com>
2024-12-09 21:34:22 +0200

    csharp: update project config

--- csharp/Othello/Version.cs
+++ csharp/Othello/Version.cs
@@ -4,8 +4,8 @@ namespace Othello
 {
     public static class Version
     {
-        public const string BuildTime = "2024-11-22_1340";
-        public const string GitBranch = "5bb76c6";
+        public const string BuildTime = "2024-12-09_2134";
+        public const string GitBranch = "01c229d";
         public const string GitCommit = "main";
         public const string VersionNumber = "2.6.0";
     }
