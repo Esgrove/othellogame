50e9ec86aaa3eba5f0e0dc5e0908fd8b5b9e7301
Esgrove <esgrove@outlook.com>
2024-12-21 12:10:28 +0200

    rust: update shadow_rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,14 @@
+use std::collections::BTreeSet;
+
+use shadow_rs::ShadowBuilder;
+
 // Make compile-time build information available.
 // https://github.com/baoyachi/shadow-rs
-fn main() -> shadow_rs::SdResult<()> {
-    shadow_rs::new()
+
+fn main() {
+    let deny = BTreeSet::from([shadow_rs::CARGO_METADATA, shadow_rs::CARGO_TREE]);
+    ShadowBuilder::builder()
+        .deny_const(deny)
+        .build()
+        .expect("Shadow build failed");
 }
