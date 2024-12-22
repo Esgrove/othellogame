b7b3b9af6f6b963eaa1e6d8aabf2f97b3ef2e308
Esgrove <esgrove@outlook.com>
2024-12-22 23:33:49 +0200

    kotlin: convert project to use kotlin native / multiplatform

--- kotlin/app/src/main/kotlin/othello/Utils.kt
+++ kotlin/app/src/main/kotlin/othello/Utils.kt
@@ -140,20 +140,5 @@ fun calculateSha256(input: String): String {
     return hashingSink.hash.hex()
 }
 
-object BuildInfo {
-    private val properties: Properties = Properties().apply {
-        val propFileName = "build-info.properties"
-        val classLoader = javaClass.classLoader ?: ClassLoader.getSystemClassLoader()
-        classLoader.getResourceAsStream(propFileName)?.use {
-            load(it)
-        } ?: Properties()
-    }
-
-    val branch: String = properties.getProperty("build.branch", "unknown")
-    val commit: String = properties.getProperty("build.commit", "unknown")
-    val date: String = properties.getProperty("build.date", "unknown")
-    val version: String = properties.getProperty("build.version", "unknown")
-}
-
 fun versionInfo(): String =
     "${BuildInfo.version} ${BuildInfo.date} ${BuildInfo.branch} ${BuildInfo.commit}"
