1b2d80ac3ac91332a6f57e4c317d77f2f4c4e62b
Esgrove <esgrove@outlook.com>
2025-01-26 01:41:35 +0200

    cpp: fix format script for windows

--- cpp/format.sh
+++ cpp/format.sh
@@ -4,4 +4,6 @@ set -eo pipefail
 # Format C++ code using ClangFormat
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-clang-format --verbose -i -style=file:"$DIR/.clang-tidy" "$DIR"/src/*.cpp "$DIR"/src/*.hpp
+
+cd "$DIR"
+clang-format --verbose -i -style=file:".clang-format" "$DIR"/src/*.cpp "$DIR"/src/*.hpp
