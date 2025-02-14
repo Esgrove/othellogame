af515f1533b6a76c24aa69ff9c807c56954ddb6d
Esgrove <esgrove@outlook.com>
2025-02-14 18:19:55 +0200

    rust: add missing settings.rs

---
+++ rust/src/settings.rs
@@ -0,0 +1,71 @@
+//! # Settings
+//! Defines game settings
+//!
+//! Akseli Lukkarila
+//! 2019-2025
+
+use std::fmt;
+use std::fmt::Display;
+
+use crate::player::PlayerSettings;
+
+/// Game settings.
+#[derive(Debug, Copy, Clone)]
+pub struct Settings {
+    pub board_size: usize,
+    pub autoplay_mode: bool,
+    pub check_mode: bool,
+    pub show_helpers: bool,
+    pub show_log: bool,
+    pub test_mode: bool,
+    pub use_defaults: bool,
+}
+
+impl Settings {
+    /// Get player setting values from overall game settings.
+    pub const fn to_player_settings(self) -> PlayerSettings {
+        PlayerSettings {
+            show_helpers: self.show_helpers,
+            check_mode: self.check_mode,
+            test_mode: self.test_mode,
+        }
+    }
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            board_size: 8,
+            check_mode: false,
+            test_mode: false,
+            autoplay_mode: false,
+            use_defaults: false,
+            show_helpers: true,
+            show_log: false,
+        }
+    }
+}
+
+impl Display for Settings {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "\
+Settings:
+  board_size: {}
+  autoplay: {}
+  check_mode: {}
+  use_defaults: {}
+  show_helpers: {}
+  show_log: {}
+  test_mode: {}",
+            self.board_size,
+            self.autoplay_mode,
+            self.check_mode,
+            self.use_defaults,
+            self.show_helpers,
+            self.show_log,
+            self.test_mode
+        )
+    }
+}
