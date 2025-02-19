77b20a6e620cf7da30c786b2fc97184ec1f11c06
Esgrove <esgrove@outlook.com>
2025-02-19 11:41:53 +0200

    kotlin: doc formatting

--- kotlin/app/src/main/kotlin/othello/Utils.kt
+++ kotlin/app/src/main/kotlin/othello/Utils.kt
@@ -9,14 +9,14 @@ const val MIN_BOARD_SIZE = 4
 const val MAX_BOARD_SIZE = 10
 const val DEFAULT_BOARD_SIZE = 8
 
-/** Represents one game piece or lack of one.*/
+/** Represents one game piece or lack of one. */
 enum class Disk(val value: Int) {
     Black(-1),
     Empty(0),
     White(1),
 }
 
-/** Represents a step direction on the board.*/
+/** Represents a step direction on the board. */
 data class Step(val x: Int, val y: Int) {
     override fun hashCode(): Int {
         var result = 17
@@ -38,7 +38,7 @@ data class Step(val x: Int, val y: Int) {
     override fun toString(): String = "[$x,$y]"
 }
 
-/** Represents one square location on the board.*/
+/** Represents one square location on the board. */
 data class Square(val x: Int, val y: Int) : Comparable<Square> {
     override fun hashCode(): Int {
         var result = 17
@@ -70,7 +70,7 @@ data class Square(val x: Int, val y: Int) : Comparable<Square> {
     override fun toString(): String = "($x,$y)"
 }
 
-/** Represents one possible disk placement for given disk color.*/
+/** Represents one possible disk placement for given disk color. */
 data class Move(val square: Square, val value: Int, val disk: Disk, val directions: List<Step>) :
     Comparable<Move> {
     override fun compareTo(other: Move): Int = when {
@@ -84,39 +84,42 @@ data class Move(val square: Square, val value: Int, val disk: Disk, val directio
     override fun toString(): String = "Square: $square -> value: $value"
 }
 
-/** Returns the print colour for the given Disk.*/
+/** Returns the print colour for the given Disk. */
 fun Disk.diskColor(): AnsiColor = when (this) {
     Disk.Empty -> AnsiColor.WHITE
     Disk.White -> AnsiColor.CYAN
     Disk.Black -> AnsiColor.MAGENTA
 }
 
-/** Returns string character representing board status (black, white, empty).*/
+/** Returns string character representing board status (black, white, empty). */
 fun Disk.boardChar(color: Boolean = true): String = when (this) {
     Disk.Empty -> "_"
     Disk.White -> if (color) getColor("W", this.diskColor()) else "W"
     Disk.Black -> if (color) getColor("B", this.diskColor()) else "B"
 }
 
-/** Returns the disk formatted as a coloured string.*/
+/** Returns the disk formatted as a coloured string. */
 fun Disk.name(): String = getColor(this.name.uppercase(), this.diskColor())
 
-/** Returns the opposing disk colour.*/
+/** Returns the opposing disk colour. */
 fun Disk.opponent(): Disk = when (this) {
     Disk.Empty -> Disk.Empty
     Disk.White -> Disk.Black
     Disk.Black -> Disk.White
 }
 
-/** Calculate SHA256 hash for the given string.*/
+/**
+ * Calculate the SHA-256 hash for the given string.
+ *
+ * @param input The input string to hash.
+ * @return The SHA-256 hash as a hexadecimal string.
+ */
 fun calculateSha256(input: String): String {
     val buffer = Buffer()
     val hashingSink = HashingSink.sha256(buffer)
     val bufferedSink = hashingSink.buffer()
 
-    bufferedSink.use { sink ->
-        sink.writeUtf8(input)
-    }
+    bufferedSink.use { sink -> sink.writeUtf8(input) }
 
     return hashingSink.hash.hex()
 }
